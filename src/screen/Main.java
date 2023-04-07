b187855c5902ca594b1d305d5d4252db9537cc11
Julian Fisla <jfislayt@gmail.com>
2023-04-06 23:49:18 -0400

    small changes

--- src/screen/Main.java
+++ src/screen/Main.java
@@ -10,6 +10,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		// INITIALIZE FRAME
+		
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
